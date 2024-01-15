c770ee608512fe0413056528c66f7ced927211f8
Romer Chia <romerchia@hotmail.com>
2024-01-14 22:10:02 -0500

    Added dr-tristan-lue-dds.html index.html - Corrected some texts header.html - Added link to index dr-tristan-lue-dds pages

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ $(function () {
 $(function () {
     $("#includeFooter").load("footer.html");
   });
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+$(function () { // wait for document re
+var scene = new ScrollMagic.Scene({
+    duration: 310, // the scene should last for a scroll distance of 100px
+    offset: 50 // start this scene after scrolling for 50px
+})
+    .setPin('#introduction') // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
+  });
